c6a909f756a5233fe1c9316f4f1f5671f3989603
milk2715093695 <miao_renke@qq.com>
2026-07-01 01:52:55 +0800

    config(site): disable timeline feature page

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -37,7 +37,7 @@ export const siteConfig: SiteConfig = {
 		friends: false, // 友链页面开关
 		projects: false, // 项目页面开关
 		skills: false, // 技能页面开关
-		timeline: true, // 时间线页面开关
+		timeline: false, // 时间线页面开关
 		albums: false, // 相册页面开关
 		devices: true, // 设备页面开关
 	},
